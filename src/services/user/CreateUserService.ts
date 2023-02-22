439182fa8a6a0e3601f3c1156e6c5ba8d7d15b2b
Matheus Rodela <46487106+matheusrodela@users.noreply.github.com>
2023-02-22 19:39:09 -0300

    cadastrando usuário e criptografando senha

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,3 +1,5 @@
+import prismaClient from "../../prisma"
+import { hash } from 'bcryptjs'
 
 interface UserRequest{
     name: string;
@@ -7,10 +9,37 @@ interface UserRequest{
 
 class CreateUserService{
     async execute({ name, email, password }: UserRequest){
-        console.log(name)
-        console.log(email)
 
-        return {ok: "SUCESSO"}
+        if(!email || email === ''){
+            throw new Error('Email inválido');
+        }
+
+        const userAlreadyExists = await prismaClient.user.findFirst({
+            where:{
+                email: email
+            }
+        })
+
+        if(userAlreadyExists){
+            throw new Error('Email já cadastrado');
+        }
+
+        const passwordHash = await hash(password, 8)
+
+        const user = await prismaClient.user.create({
+            data:{
+                name: name,
+                email: email,
+                password: passwordHash
+            },
+            select:{
+                id: true,
+                name: true,
+                email: true,
+            }
+        })
+
+        return user;
     }
 }
 

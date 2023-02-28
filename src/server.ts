2c5097e0749b89b566d617e212850e82ca2bea02
Matheus Rodela <46487106+matheusrodela@users.noreply.github.com>
2023-02-28 15:00:29 -0300

    correção cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response, NextFunction } from 'express'
 import 'express-async-errors'
-import cors  from 'cors';
 import { router } from './routes'
 
 const app = express();
 
+const cors = require('cors')
+
 app.use(express.json());
+app.use(cors());
 
 
 app.use(router);
